f6429d8aebe9f59099414fd9dc786388d28b69d4
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2025-10-15 12:01:25 +0200

    Hanlde MPEG-TS timestamps rollovers

--- lib/ex_hls/demuxing_engine/mpeg_ts.ex
+++ lib/ex_hls/demuxing_engine/mpeg_ts.ex
@@ -2,12 +2,18 @@ defmodule ExHLS.DemuxingEngine.MPEGTS do
   @moduledoc false
   @behaviour ExHLS.DemuxingEngine
 
+  use Bunch.Access
+
   require Logger
   alias Membrane.{AAC, H264, RemoteStream}
   alias MPEG.TS.Demuxer
 
   @enforce_keys [:demuxer]
-  defstruct @enforce_keys
+  defstruct @enforce_keys ++
+              [last_packet_ts: %{pts: nil, dts: nil}, ts_rollovers_count: 0]
+
+  # @timestamp_range_size is 2^33
+  @timestamp_range_size 8_589_934_592
 
   @type t :: %__MODULE__{
           demuxer: Demuxer.t()
@@ -72,6 +78,10 @@ defmodule ExHLS.DemuxingEngine.MPEGTS do
   @impl true
   def pop_chunk(%__MODULE__{} = demuxing_engine, track_id) do
     with {[packet], demuxer} <- Demuxer.take(demuxing_engine.demuxer, track_id) do
+      {demuxing_engine, packet} =
+        %{demuxing_engine | demuxer: demuxer}
+        |> handle_possible_timestamps_rollover(packet)
+
       chunk = %ExHLS.Chunk{
         payload: packet.data,
         pts_ms: packet.pts |> packet_ts_to_millis(),
@@ -83,7 +93,7 @@ defmodule ExHLS.DemuxingEngine.MPEGTS do
         }
       }
 
-      {:ok, chunk, %{demuxing_engine | demuxer: demuxer}}
+      {:ok, chunk, demuxing_engine}
     else
       {[], demuxer} ->
         {:error, :empty_track_data, %{demuxing_engine | demuxer: demuxer}}
@@ -98,4 +108,38 @@ defmodule ExHLS.DemuxingEngine.MPEGTS do
     demuxer = Demuxer.end_of_stream(demuxing_engine.demuxer)
     %{demuxing_engine | demuxer: demuxer}
   end
+
+  defp handle_possible_timestamps_rollover(%__MODULE__{} = demuxing_engine, packet) do
+    last_ts = state.last_packet_ts.dts || state.last_packet_ts.pts
+    rollovers_offset = demuxing_engine.ts_rollovers_count * @timestamp_range_size
+
+    packet =
+      packet
+      |> Map.update!(:pts, &add_offset_if_not_nil(&1, rollovers_offset))
+      |> Map.update!(:dts, &add_offset_if_not_nil(&1, rollovers_offset))
+
+    {demuxing_engine, packet} =
+      if last_ts != nil and last_ts > (packet.dts || packet.pts) do
+        demuxing_engine = demuxing_engine |> Map.update!(:ts_rollovers_count, &(&1 + 1))
+
+        packet =
+          packet
+          |> Map.update!(:pts, &add_offset_if_not_nil(&1, @timestamp_range_size))
+          |> Map.update!(:dts, &add_offset_if_not_nil(&1, @timestamp_range_size))
+
+        {demuxing_engine, packet}
+      else
+        {demuxing_engine, packet}
+      end
+
+    demuxing_engine =
+      demuxing_engine
+      |> put_in([:last_packet_ts, :pts], packet.pts)
+      |> put_in([:last_packet_ts, :dts], packet.dts)
+
+    {demuxing_engine, packet}
+  end
+
+  defp add_offset_if_not_nil(nil, _offset), do: nil
+  defp add_offset_if_not_nil(value, offset), do: value + offset
 end
