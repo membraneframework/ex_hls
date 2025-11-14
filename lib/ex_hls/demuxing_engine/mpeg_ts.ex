c2a37a378627109223486ac61959a22e06b7b867
Łukasz Kita <lukasz.kita0@gmail.com>
2025-11-14 10:51:34 +0100

    Make sure encoding is equal to 3

--- lib/ex_hls/demuxing_engine/mpeg_ts.ex
+++ lib/ex_hls/demuxing_engine/mpeg_ts.ex
@@ -125,10 +125,13 @@ defmodule ExHLS.DemuxingEngine.MPEGTS do
   end
 
   defp parse_tden_tag(payload) do
+    # UTF-8 encoding
+    encoding = 3
+
     with {pos, _len} <- :binary.match(payload, "TDEN"),
          <<_skip::binary-size(pos), "TDEN", tden::binary>> <- payload,
-         <<size::integer-size(4)-unit(8), _flags::16, _3, text::binary-size(size - 2), 0,
-           _rest::binary>> <- tden do
+         <<size::integer-size(4)-unit(8), _flags::16, ^encoding::8, text::binary-size(size - 2),
+           0, _rest::binary>> <- tden do
       text
     else
       _error -> nil
