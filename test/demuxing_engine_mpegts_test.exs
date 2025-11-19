e84bebbb291619c30e38915c50370c87a6991483
Łukasz Kita <lukasz.kita0@gmail.com>
2025-11-19 13:23:03 +0100

    Adjust to mpeg_ts v3 API (#17)
    * Bump dependency to mpeg_ts v3.3.5
    
    * Adjust MPEG-TS demuxing engine to api of kim_mpeg_ts v3
    
    * Remove unused mock dependency

--- test/demuxing_engine_mpegts_test.exs
+++ test/demuxing_engine_mpegts_test.exs
@@ -1,8 +1,5 @@
 defmodule ExHLS.DemuxingEngine.MPEGTS.Test do
   use ExUnit.Case, async: false
-  import Mock
-
-  alias MPEG.TS.Demuxer
 
   test "handling timestamp rollovers" do
     timestamp_range = (2 ** 33 * 1_000_000_000) |> div(90_000)
@@ -16,52 +13,41 @@ defmodule ExHLS.DemuxingEngine.MPEGTS.Test do
 
         %{
           og_timestamp: og_timestamp,
-          pts: rolled_timestamp,
-          dts: rolled_timestamp,
-          data: <<>>,
-          discontinuity: false,
-          is_aligned: true
+          payload: %{
+            pts: rolled_timestamp,
+            dts: rolled_timestamp,
+            data: <<>>,
+            discontinuity: false,
+            is_aligned: true
+          }
         }
       end)
 
-    demuxer = %{
-      waiting_random_access_indicator: nil,
-      packet_buffers: %{1 => packets, 2 => packets},
-      pmt: %{streams: %{}}
+    demuxing_engine = ExHLS.DemuxingEngine.MPEGTS.new(0)
+
+    demuxing_engine = %{
+      demuxing_engine
+      | packets_map: %{1 => Qex.new(packets), 2 => Qex.new(packets)}
     }
 
-    new = fn -> demuxer end
+    [1, 2]
+    |> Enum.reduce(demuxing_engine, fn track_id, demuxing_engine ->
+      {chunks, demuxing_engine} =
+        1..200
+        |> Enum.map_reduce(demuxing_engine, fn _i, demuxing_engine ->
+          {:ok, chunk, demuxing_engine} =
+            ExHLS.DemuxingEngine.MPEGTS.pop_chunk(demuxing_engine, track_id)
 
-    take = fn demuxer, track_id ->
-      demuxer
-      |> get_and_update_in(
-        [:packet_buffers, track_id],
-        fn [head | tail] -> {[head], tail} end
-      )
-    end
-
-    with_mock Demuxer, new: new, take: take do
-      demuxing_engine = ExHLS.DemuxingEngine.MPEGTS.new(0)
-
-      [1, 2]
-      |> Enum.reduce(demuxing_engine, fn track_id, demuxing_engine ->
-        {chunks, demuxing_engine} =
-          1..200
-          |> Enum.map_reduce(demuxing_engine, fn _i, demuxing_engine ->
-            {:ok, chunk, demuxing_engine} =
-              ExHLS.DemuxingEngine.MPEGTS.pop_chunk(demuxing_engine, track_id)
-
-            {chunk, demuxing_engine}
-          end)
-
-        Enum.zip(chunks, packets)
-        |> Enum.each(fn {chunk, packet} ->
-          assert chunk.pts_ms == div(packet.og_timestamp, 1_000_000)
-          assert chunk.dts_ms == div(packet.og_timestamp, 1_000_000)
+          {chunk, demuxing_engine}
         end)
 
-        demuxing_engine
+      Enum.zip(chunks, packets)
+      |> Enum.each(fn {chunk, packet} ->
+        assert chunk.pts_ms == div(packet.og_timestamp, 1_000_000)
+        assert chunk.dts_ms == div(packet.og_timestamp, 1_000_000)
       end)
-    end
+
+      demuxing_engine
+    end)
   end
 end
