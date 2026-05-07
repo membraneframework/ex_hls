e745bb3760cf9886251261b5f4fc962b36900a0c
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:02 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -44,16 +44,15 @@ defmodule ExHLS.Mixfile do
       {:membrane_mp4_plugin, "~> 0.36.0"},
       {:membrane_h26x_plugin, "~> 0.10.2"},
       {:mpeg_ts, "~> 3.3.5"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:mix, :syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
